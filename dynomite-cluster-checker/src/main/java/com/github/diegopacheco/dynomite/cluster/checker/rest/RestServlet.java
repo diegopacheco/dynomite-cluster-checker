1f795cd1b0506aab72a245598e03fcc2345cf56c
diegopacheco <diego.pacheco.it@gmail.com>
2017-04-27 18:16:27 -0700

    upgrade to dyno 1.5.8-rc.4 - Remove hsytrix deps

--- dynomite-cluster-checker/src/main/java/com/github/diegopacheco/dynomite/cluster/checker/rest/RestServlet.java
+++ dynomite-cluster-checker/src/main/java/com/github/diegopacheco/dynomite/cluster/checker/rest/RestServlet.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 
 import com.github.diegopacheco.dynomite.cluster.checker.tasks.engine.DCCTaskExecutionEngine;
-import com.netflix.hystrix.Hystrix;
 
 @SuppressWarnings("serial")
 public class RestServlet extends HttpServlet {
@@ -39,7 +38,6 @@ public class RestServlet extends HttpServlet {
 			logger.error(e); 
 			out.write("{ \"Error\": \"" + e.toString() + " - " + org.apache.commons.lang3.exception.ExceptionUtils.getStackTrace(e) + "\" }");
 		}finally {
-			Hystrix.reset();
 			out.close();
 		}
 	}
